4b81bbdfe6abfb18473547baa3fdff8780e1d68d
Maksym-Sirenko <toyotasan@gmail.com>
2025-07-26 15:31:56 +0300

    first task

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,28 @@
+import SimpleLightbox from 'simplelightbox';
 
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items.js';
+
+const galleryElem = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery-item">
+	<a class="gallery-link" href="${original}">
+		<img 
+		  class="gallery-image" 
+		  src="${preview}" 
+		  alt="${description}" 
+		/>
+	</a>
+</li>`;
+  })
+  .join('');
+
+galleryElem.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
